33c58e9e478d047eaa1a055159486c9ff0e61f21
yoan1214 <bermudezyoancarlos@gmail.com>
2024-02-06 17:42:36 +0100

    inicio

--- src/main/java/controlador/BibliotecaController.java
+++ src/main/java/controlador/BibliotecaController.java
@@ -29,7 +29,7 @@ public class BibliotecaController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("Servlet BibliotecaController");
+		System.out.println("Servlet BibliotecaController hola");
 		RequestDispatcher despachador = null;
 		despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
 		despachador.forward(request, response);
